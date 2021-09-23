7b6b6309f0f9cf02534331c11463e4577037c29b
Olga Kupryjaniuk <olga.kupryjaniuk@gmail.com>
2021-09-23 18:00:46 +0200

    class name changed

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <div className="section__headerWrapper">
-            <h2 className="section__buttonHeader">{title}</h2>
+            <h2 className="section__headerName">{title}</h2>
             {extraHeaderContent}
         </div>
         {body}
